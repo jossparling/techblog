e0b8588a55aa235eac80298e274db2ff22dddbb3
jossparling <jossparling@gmail.com>
2022-08-08 12:33:59 -0500

    added models & initalized db

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Post = require('./Post');
+const User = require('./User');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  module.exports = { User, Post };
